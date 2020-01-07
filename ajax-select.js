33d5bbb3e21620a08ef908f8a4a6e1578abe34db
Dhruv Joshi <joshidhruv997@gmail.com>
2020-01-07 16:24:31 +0530

    invoke web service in the component's url to generate and append the option elements list

--- ajax-select.js
+++ ajax-select.js
@@ -15,6 +15,20 @@ class AJAXSelect extends HTMLElement {
   connectedCallback() {
     const selectElem = this.shadowRoot.querySelector('select');
     selectElem.setAttribute('name', this.name);
+
+    let xhr = new XMLHttpRequest();
+    xhr.onload = () => {
+      if(xhr.status >= 200 && xhr.status < 300) {
+        let response = JSON.parse(xhr.response);
+        for(let x in response) {
+          let optionElem = document.createElement('option');
+          optionElem.text = response[x];
+          selectElem.add(optionElem);
+        }
+      }
+    };
+    xhr.open('GET', this.url);
+    xhr.send();
   }
 
   get name() {
@@ -23,6 +37,10 @@ class AJAXSelect extends HTMLElement {
 
   set name(value) {
     this.setAttribute('name', value);
+  }
+
+  get url() {
+    return this.getAttribute('url');
   }
 }
 
