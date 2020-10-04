228cb57ad3af847c725bbf89e4412e2877116b7f
Dhruv Joshi <joshidhruv997@gmail.com>
2020-10-04 21:54:19 +0530

    provide usage instructions for ajax-select webcomponent

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,6 @@
+// code highlighter
+hljs.initHighlightingOnLoad();
+
 // service response is passed as callback function's argument
 function countrySelector(response) {
   // this references the select element to which callback function is attached
