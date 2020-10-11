ddc1f29db59fef576c3b83dedf36e695faede4d5
Dhruv Joshi <joshidhruv997@gmail.com>
2020-10-11 15:29:51 +0530

    responsive site design with custom scroll behavior and linked latest jquery version

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,23 @@ function countrySelector(response) {
   // this references the select element to which callback function is attached
   response.forEach(element => this.addOption(element['name'], element['numericCode']));
 }
+
+function scrollTo(clickedLink) {
+  var elementClass = clickedLink.attr('href').replace('#', '.');
+  var scrollTo = 0;
+  if(elementClass != '.top-content') {
+    elementClass += '-container';
+    scrollTo = $(elementClass).offset().top;
+  }
+  if($(window).scrollTop() != scrollTo) {
+    $('html, body').stop().animate({scrollTop: scrollTo}, 1000);
+  }
+}
+
+$(document).ready(function() {
+  $('a.scroll-link').on('click', function(e) {
+    e.preventDefault();
+    scrollTo($(this));
+  });
+  new WOW().init();
+});
